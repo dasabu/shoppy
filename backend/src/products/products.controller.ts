90a323eab5a27fdd2b77fcec9484fc28983ae44f
duyanhhh1811 <duyanhlucas302@gmail.com>
2025-02-20 23:00:29 +0700

    feat: add upload controller

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -1,10 +1,24 @@
 import { TokenPayload } from '../auth/token-payload.interface';
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { CreateProductRequest } from './dto/create-product.request';
 import { CurrentUser } from '../auth/current-user.decorator';
-
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { Request } from 'express';
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
@@ -17,6 +31,51 @@ export class ProductsController {
   ) {
     return this.productsService.createProduct(body, user.userId);
   }
+
+  @Post(':productId/image')
+  @UseGuards(JwtAuthGuard)
+  /**
+   * Interceptor: execute some code before executing route handler
+   * as well as after (using Observation)
+   *
+   * @FileInterceptor(): config multer to parse incoming form data request
+   * and extract file
+   *
+   * 'image': key inside coming form data object contain actual file
+   */
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: 'public/products',
+        // custom filename for incoming file
+        filename: (
+          req: Request,
+          file: Express.Multer.File,
+          callback: (error: null, filename: string) => void,
+        ) => {
+          callback(
+            null,
+            `${req.params.productId}${extname(file.originalname)}`,
+          );
+        },
+      }),
+    }),
+  )
+  uploadProductImage(
+    /**
+     * validation for incoming file
+     * Pipe: transform a http request or provide validation
+     */
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 500000 }),
+          new FileTypeValidator({ fileType: 'image/jpeg' }),
+        ],
+      }),
+    )
+    _file: Express.Multer.File,
+  ) {}
 
   @Get()
   @UseGuards(JwtAuthGuard)
